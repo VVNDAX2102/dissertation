86a0e5b398ab207d3398f31226635736256c8ef1
VVNDAX2102 <vivianvincentdas@gmail.com>
2025-08-16 08:26:09 +0530

    Initial commit

---
+++ downloader/download.py
@@ -0,0 +1,30 @@
+import yfinance as yf
+import datetime
+import os
+
+# Set up dates
+end_date = datetime.datetime.today()
+start_date = end_date - datetime.timedelta(days=5*365)
+
+ticker = 'ITBEES.NS'
+
+# Download data
+df = yf.download(
+    tickers=ticker,
+    start=start_date.strftime("%Y-%m-%d"),
+    end=end_date.strftime("%Y-%m-%d"),
+    interval="1d",
+    auto_adjust=True,
+    progress=False
+)
+
+# Reset index so Date becomes a column
+df.reset_index(inplace=True)
+
+# Build save path one folder outside, inside 'data/India'
+save_path = os.path.join('technology.csv')
+
+# Save CSV
+df.to_csv(save_path, index=False)
+
+print(f"Data saved to: {os.path.abspath(save_path)}")
