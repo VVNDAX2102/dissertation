8bbbe6cdfff3a6a99833c8a952b1bb11aa127a1b
I-am-Sagar <sagar.l.udasi@gmail.com>
2025-08-16 08:58:05 +0100

    Add introduction prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import yaml
+import os
 
 if __name__ == "__main__":
     
@@ -10,3 +11,8 @@ if __name__ == "__main__":
     plots_path = config['plots_path']
 
     # print(data_path, results_path, plots_path)
+
+    os.makedirs(os.path.join(results_path, 'trade_books'), exist_ok=True)
+    os.makedirs(os.path.join(results_path, 'reports'), exist_ok=True)
+
+    # STEP 1: READ THE DATA
